fd73b0778b93acba01717c8ba73d50f25fa66069
LTB Lizzy <allenderse@gmail.com>
2021-07-06 01:10:11 +1000

    commit final consolidated code that actually works

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,115 +1,278 @@
-var startButton = document.getElementById('startButton')
-var submitButton = document.getElementById('submitButton')
-var questionContainerEl = document.getElementById('questionContainer')
-var questionEl = document.getElementById('question')
-var answerButtonsEl = document.getElementById('answerButtons')
-var count = 60;
-var timer;
-var questions = [];
+// When user clicks the start button, then a timer starts and user is presented with a question
 
+    // Declare global variables 
+    var startBtn = document.getElementById("startBtn");
+    var time = 60;
+    var time_remaining = true;
+    var time_start= false;
+    var countdownTimer = document.getElementById("countdownTimer");
+    var homeContainer =  document.getElementById("homeContainer");
+    var quizContainer = document.getElementById("quizContainer");
+    var questionHeading = document.getElementById("questionHeading");
+    var answerChoiceA = document.getElementById("answerChoiceA");
+    var answerChoiceB = document.getElementById("answerChoiceB");
+    var answerChoiceC = document.getElementById("answerChoiceC");
+    var answerChoiceD = document.getElementById("answerChoiceD");
+    var correctAnswer = document.getElementById("correctAnswer");    
+    var high_scores= [];
+    var output="";
+    // Set score = 0 at the start of the game 
+    var score = 0;
+    // question index
+    let i = 0;
 
-startButton.addEventListener('click', startGame)
+// QUESTIONS ARRAY:
 
-function startGame() {
-startButton.classList.add('hide');
-submitButton.classList.remove('hide');
-questionContainerEl.classList.remove('hide');
-setNextQuestion()
-startClock()
-}
+var questionsArray = [
+{
+    question: "Question: What does the acronym CSS stand for?",
+    imageSrc: "",
+    answerChoice: ["A) Concatenated Software Styles", "B) Core Software Styles", "C) Sassy Style Sheets", "D) Cascading Style Sheets"],
+    correctAnswer: 3
+}, 
+{
+    question: "Question: Which of the following is a CSS class?",
+    imageSrc: "",
+    answerChoice: ["A) .important", "B) #important", "D) !important", "D) $important"],
+    correctAnswer: 0
+},
+{
+    question: "Question: Where is a global variable declared in Javascript?",
+    imageSrc: "",
+    answerChoice: ["A) inside a function", "B) at the end of the document ", "C) in the main body of your code", "D) in the document header"],
+    correctAnswer: 2
+}, 
+{
+    question: "Question: What is the html tag used to create an unordered list?",
+    imageSrc: "",
+    answerChoice: ["A) <list>", "B) <ulist>", "C) <unorderedList>", "D) <ul>"],
+    correctAnswer: 3
+},
+];
 
-function startClock() {
-    $("counter").text(count);
-    timer = setTimeout(update,1000);
-    function update(){
-        if (count > 0) {
-            $("#counter").text(--count);
-            timer = setTimeout(update,1000);
-        } else {
-            alert ("Time up. You lose!!")
-        } }
-    }
+//COUNTDOWN TIMER FUNCTION: set countdown timer and interval. Set time-related valiables.
 
-function setNextQuestion() {
-    for (let i = 0; i < questions.length; i++) {
-        result = questions[i];
-        showQuestion(result)
-    }
+//change the seconds variable every second.
+var countdownTimerInterval = setInterval(setCountdownTimer, 1000);
 
-
-
-function showQuestion(question) {
-    questionEl.innerText = question.question;
-    question.answers.forEach(answer => {
-        button = document.createElement('button');
-        button.innerText = answer.text;
-        button.classList.add('button');
-        if (answer.correct) {
-            button.dataset.correct = answer.correct;
+//function that changes the time var
+function setCountdownTimer() {
+        if (time_start)
+        time--;
+        if(time<= 0) {
+        end_quiz();
+        time = 0;    
+        // clearInterval(countdownTimerInterval);
+        //alert user and stop quiz
         }
-        button.addEventListener('click', selectAnswer);
-        answerButtonsEl.appendChild(button);
-    })
-}
-
-function selectAnswer(e) {
-    var button = e.target;
-    var correct = button.dataset.correct;
-    setStatusClass(document.body, correct);
-    Array.from(answerButtonsEl.children).forEach(button => {
-        setStatusClass(button.dataset.correct);
-    })
-}
-
-function setStatusClass(element, correct) {
-    if (correct) {
-        element.classList.add('correct')
-    } else {
-        element.classList.add('wrong')
+        document.getElementById("timer").innerHTML = time;
     }
-}
-}
 
-function submit() {
+// START EVENT LISTENER: When user clicks Start button, start the countdown timer and quiz questions. Add an event listener to each button.
+startBtn.addEventListener("click", function() {
+    quizContainer.style.display = "block";
+    homeContainer.style.display ="none";
+    countdownTimer.style.display= "block";
+    document.getElementById("score_keeper").style.display= "block";
+    document.getElementById("score").innerHTML = score;
+    setCountdownTimer();
+    setQuizQuestions();
+    time_start= true;
+});
 
-}
+// QUESTIONS FUNCTION: display questions and multiple-choice answers
 
-var questions = 
-[
-    {
-       question: 'What does the acronym CSS stand for?',
-       answers: [
-           {text: 'Concatenated Software Styles', correct: false},
-           {text: 'Core Style Sheets', correct: false},
-           {text: 'Crazy Sassy Styles', correct: false},
-           {text: 'Cascading Style Sheets', correct: true},
-       ]
-    },
-    {
-        question: 'Which of the following is a CSS class?',
-        answers: [
-            {text: '#important', correct: false},
-            {text: '.important', correct: true},
-            {text: '!important', correct: false},
-            {text: '"important"', correct: false},
-        ]
-     },
-     {
-        question: 'Where is a global variable declared in Javascript?',
-        answers: [
-            {text: 'inside a function', correct: false},
-            {text: 'at the end of the document', correct: false},
-            {text: 'in the main body of your code', correct: true},
-            {text: 'in the document header', correct: false},
-        ]
-     },
-     {
-        question: 'What is the html tag used to create an unordered list?',
-        answers: [
-            {text: '<list>', correct: false},
-            {text: '<ulist>', correct: false},
-            {text: '<unorderedList>', correct: false},
-            {text: '<ul>', correct: true},
-        ]
-     },
-]
+function setQuizQuestions() {
+        questionHeading.textContent = questionsArray[i].question;
+        answerChoiceA.textContent = questionsArray[i].answerChoice[0]; 
+        answerChoiceB.textContent = questionsArray[i].answerChoice[1]; 
+        answerChoiceC.textContent = questionsArray[i].answerChoice[2]; 
+        answerChoiceD.textContent = questionsArray[i].answerChoice[3]; 
+        };
+
+// When user answers a question: then user is presented with another question
+
+// Store user answer choices. Clear elements and update score count.
+
+// Change to next question
+answerChoiceA.addEventListener('click', function(event) {
+        event.stopPropagation();
+        correctAnswer= questionsArray[i].correctAnswer;
+        console.log("correctAnswer " + correctAnswer);
+        // check answer
+        if (0 === correctAnswer) { 
+            // display message to user for 1  second stating if the answer is correct or incorrect
+            document.getElementById("AnswerResponse").innerHTML = "Correct!";
+            setTimeout(function() {
+            document.getElementById("AnswerResponse").innerHTML = "";
+                },
+                1000
+            );
+            // when user answers a question correctly, increase the score
+            score++;    
+            // display updated score progress
+            document.getElementById("score").innerHTML = score;
+        } else {
+            countdownTimerInterval;
+            // when user answers a question inccorrectly, subtract from the time
+            document.getElementById("AnswerResponse").innerHTML = "Incorrect!";
+            setTimeout(function() {
+                document.getElementById("AnswerResponse").innerHTML = "";
+                    },
+                    1000
+                );
+        }
+        if (i >= questionsArray.length -1) {
+        end_quiz();
+        } else {
+            i++ 
+            setQuizQuestions();
+        };
+    });
+
+answerChoiceB.addEventListener('click', function(event) {
+    event.stopPropagation();
+    correctAnswer = questionsArray[i].correctAnswer;
+    console.log(correctAnswer);
+        if (1 === correctAnswer) { 
+            document.getElementById("AnswerResponse").innerHTML = "Correct!";
+            setTimeout(function() {
+                document.getElementById("AnswerResponse").innerHTML = "";
+                    },
+                    1000
+                );
+            score++;
+            document.getElementById("score").innerHTML = score;
+        } else {
+            time-10;
+            document.getElementById("AnswerResponse").innerHTML = "Incorrect!";
+            setTimeout(function() {
+                document.getElementById("AnswerResponse").innerHTML = "";
+                    },
+                    1000
+                );
+        }
+        if (i >= questionsArray.length -1) {
+        end_quiz();
+        } else {
+         i++ 
+        setQuizQuestions();
+        };
+    });
+
+answerChoiceC.addEventListener('click', function(event) {
+    event.stopPropagation();
+    correctAnswer = questionsArray[i].correctAnswer;
+    console.log(correctAnswer);
+    if (2 === correctAnswer) { 
+        document.getElementById("AnswerResponse").innerHTML = "Correct!";
+        setTimeout(function() {
+            document.getElementById("AnswerResponse").innerHTML = "";
+                },
+                1000
+            );
+        score++;
+        document.getElementById("score").innerHTML = score;
+    } else {
+            time--;
+        document.getElementById("AnswerResponse").innerHTML = "Incorrect!";
+        setTimeout(function() {
+            document.getElementById("AnswerResponse").innerHTML = "";
+                },
+                1000
+            );
+    }
+    if (i >= questionsArray.length -1) {
+    end_quiz();
+    } else {
+        i++ 
+        setQuizQuestions();
+    };
+    });
+
+    answerChoiceD.addEventListener('click', function(event) {
+        event.stopPropagation();
+        correctAnswer = questionsArray[i].correctAnswer;
+        console.log(correctAnswer);
+        if (3 === correctAnswer) { 
+            document.getElementById("AnswerResponse").innerHTML = "Correct!";
+            setTimeout(function() {
+                document.getElementById("AnswerResponse").innerHTML = "";
+                    },
+                    1000
+                );
+            score++;
+            document.getElementById("score").innerHTML = score;
+        } else {
+            time-10;
+            document.getElementById("AnswerResponse").innerHTML = "Incorrect!";
+            setTimeout(function() {
+                document.getElementById("AnswerResponse").innerHTML = "";
+                    },
+                    1000
+                );
+        }
+        if (i >= questionsArray.length -1) {
+        end_quiz();
+        } else {
+            i++ 
+            setQuizQuestions();
+        };
+});
+
+        //end quiz
+        function end_quiz(){
+            document.getElementById("game_over").style.display= "block";
+            document.getElementById("quizContainer").style.display="none";
+            document.getElementById("countdownTimer").style.display= "none";
+            document.getElementById("score_keeper").style.display= "none";
+            document.getElementById("AnswerResponse").innerHTML="";
+            document.getElementById("end_score").innerHTML= score;
+            }
+
+        //submit score and initals
+            function submit_score() {
+             high_scores.push(document.getElementById("initials").value + " " + score);
+             view_high_scores();
+            }
+
+        // localStorage.setItem("score",JSON.stringify(AnswerResponse));
+        // localStorage.setItem("initials", JSON.stringify(initials));
+        
+        function view_high_scores(){
+        
+        // changing the screen output
+            document.getElementById("quizContainer").style.display="none";
+            document.getElementById("game_over").style.display= "none";
+            document.getElementById("high_scores_page").style.display="block";
+        
+            output="";
+            for(let k=0; k<high_scores.length; k++){
+                 output = output + "  " + high_scores[k];
+            }
+            document.getElementById("high_scores").innerHTML= output;                
+             clear_up();
+        }
+
+        // refresh the site to the home container page
+        function go_home(){	
+                document.getElementById("high_scores_page").style.display= "none";
+                document.getElementById("homeContainer").style.display= "block";
+                clear_up();
+        }
+        
+        // clear the highscore
+        function clear_hs(){
+            high_scores = [];
+            // high_scores.splice(0, high_scores.length);
+          }
+        
+        // refresh the site 
+        function clear_up(){
+        
+        time=60;
+        time_remaining=true;
+        time_start=false;
+        i=0;
+        score=0;
+        }
